5649651842edd37dd3eb82fa83581c590cbd68b5
where-are-the-avocados <phyrxxne@outlook.com>
2024-03-04 20:08:41 -0300

    improvements

--- Test.py
+++ Test.py
@@ -1,14 +1,32 @@
-print("Bienvenido a la calculadora de IVA")
-print("Permite extraer la cantidad de IVA y monto neto de un valor asignado")
-print("Las opciones son \"neto\" y \"bruto\", para ello entregará el IVA correspondiente y el monto neto o bruto según corresponda")
-print("Para salir, escribe \"salir\"")
+MENSAJE = """
+    ---Bienvenido a la calculadora de IVA---
+
+    Permite extraer la cantidad de IVA y monto neto de un valor asignado.
+
+    Las opciones son \"neto\" y \"bruto\", para ello indica el IVA correspondiente y el monto neto o bruto según corresponda.
+
+    Para ayuda, escriba \"Help\"
+    Para salir, escribe \"salir\"
+    """
+print(MENSAJE)
 
 resultado = ""
 while True:
     if not resultado:
         resultado = input("Ingrese valor: $ ")
         if resultado.lower() == "salir":
+            print("Ha finalizado el programa")
             break
+        elif resultado.lower() == "help":
+            AYUDA = """
+            Neto = Valor sin el IVA 
+            Bruto = Valor total con el IVA
+            
+            Neto + Neto * 0.19 = Bruto
+            """
+            print(AYUDA)
+            break
+
         iva = round(float(resultado))
         resultado = round(float(resultado))
         resultado1 = round(float(resultado))
@@ -20,11 +38,13 @@ while True:
     if op.lower() == "neto":
         resultado1 *= 0.19
         bruto = resultado * 1.19
+
     elif op.lower() == "bruto":
         resultado2 /= 1.19
         iva = resultado2 * 0.19
+
     else:
-        print("Operación invalida")
+        print("Operación inválida")
         break
 
     if op.lower() == "neto":
